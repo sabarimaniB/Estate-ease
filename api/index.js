ed235981295ff8d9e0a70d1ea4f7f50b5af58a94
sabarimaniB <sabarimani.b2022eee@sece.ac.in>
2025-10-06 20:17:52 +0530

    Fix CORS for frontend

--- api/index.js
+++ api/index.js
@@ -15,33 +15,33 @@ mongoose
   .then(() => console.log('✅ Connected to MongoDB!'))
   .catch((err) => console.log(err));
 
-const __dirname = path.resolve();
-const app = express();
-app.use(cors({
-        origin: ["https://estate-ease-1-l3ba.onrender.com","http://localhost:5173"] ,
-        credentials: true,
-    }
-))
-//app.options('*', cors())
+  const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
+  app.use(cors({
+    origin: [
+      "https://estate-ease-1-l3ba.onrender.com",
+      "http://localhost:5173"
+    ],
+    credentials: true,
+  }));
+  app.use(express.json());
+  app.use(cookieParser());
+  
+  app.use('/api/user', userRouter);
+  app.use('/api/auth', authRouter);
+  app.use('/api/listing', listingRouter);
+  
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+  
+  app.get('/', (req, res) => {
+    res.send('Backend working fine!');
+  });
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
 
-// ✅ Routes 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
-
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get("/", (req, res) => {
-  res.send("Backend working fine!");
-});
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
-// ✅ Global error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
